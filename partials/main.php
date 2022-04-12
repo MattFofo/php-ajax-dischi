9fe7a5e1cb00ba15494b91a1326f7f0646396c18
MattFofo <matteo.segattini2@gmail.com>
2022-04-12 15:34:58 +0200

    stile cards

--- partials/main.php
+++ partials/main.php
@@ -2,16 +2,16 @@
 // include './db.php' ?>
 
 <main>
-   <div class="container">
-      <?php foreach ($response['arr_dischi'] as $disco) { ?>
-         <div class="card">
-            <div class="card-disco bg-secondary m-3 p-4">
+   <div class="container-fluid">
+      <div class="row justify-content-center">
+         <?php foreach ($response['arr_dischi'] as $disco) { ?>
+            <div class="card-disco col-4 col-sm-4 col-md-3 col-lg-2 bg-secondary m-3 p-4">
                <img class="img-fluid" src="<?= $disco['poster'] ?>" alt="<?= $disco['title'] ?>">
                <h3 class="text-center py-2"><?= $disco['title'] ?></h3>
                <h4 class="text-center"><?= $disco['author'] ?></h4>
                <h4 class="text-center"><?= $disco['year'] ?></h4>
             </div>
-         </div>
-      <?php } ?>
+         <?php } ?>
+      </div>
    </div>
 </main>
